bc5a417d7ccacde32bdbc3d048e1c13bf6ed1218
Jesper Noer <jesper@noer.gl>
2020-01-30 09:38:20 +0100

    Added RestaurantAPI

---
+++ Bestil-Mere/RestaurantAPI/Models/Restaurant.cs
@@ -0,0 +1,28 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace RestaurantAPI.Models
+{
+    public class Restaurant
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string RestaurantName { get; set; }
+       
+        public string RestaurantType { get; set; }
+        
+        public string Cvr { get; set; }
+        
+        public string Address { get; set; }
+
+        public string PostalCode { get; set; }
+
+        public string City { get; set; }
+        
+        public string Country { get; set; }
+    }
+}
