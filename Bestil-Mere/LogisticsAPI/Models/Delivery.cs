d50e736e1cecee73a0c9f89a8530ac4aad756804
Jonas Laursen <jola@cloudfactory.dk>
2020-02-07 14:04:12 +0100

    Added cost and EstimatedDelivery to the model

--- Bestil-Mere/LogisticsAPI/Models/Delivery.cs
+++ Bestil-Mere/LogisticsAPI/Models/Delivery.cs
@@ -1,8 +1,12 @@
+using System;
+
 namespace LogisticsAPI.Models
 {
     public class Delivery
     {
         public Location LocationFrom { get; set; }
         public Location LocationTo { get; set; }
+        public decimal Cost { get; set; }
+        public DateTime EstimatedDelivery { get; set; }
     }
 }
