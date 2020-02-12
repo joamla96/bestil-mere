34c96f394aa9f1367a76f0f402308aa07d7d219b
Jesper Noer <jesper@noer.gl>
2020-02-12 14:35:49 +0100

    Added messaging to RestaurantAPI

---
+++ Bestil-Mere/RestaurantAPI/Messaging/MessagingSettings.cs
@@ -0,0 +1,12 @@
+namespace RestaurantAPI.Messaging
+{
+    public class MessagingSettings : IMessagingSettings
+    {
+        public string ConnectionString { get; set; }
+    }
+    
+    public interface IMessagingSettings
+    {
+        string ConnectionString { get; set; }
+    }
+}
