27828d0c52be49c4e484b97eda2a78c64b0c5ba7
Jonas Laursen <jona8690@easv365.dk>
2020-02-20 10:46:35 +0100

    Added Authentication service

---
+++ Bestil-Mere/AuthAPI/Controllers/Auth.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using AuthAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthAPI.Controllers
+{
+    public class Auth : Controller
+    {
+        private JwtHandler _jwt;
+        private ICollection<User> _users;
+
+        public Auth(JwtHandler jwt)
+        {
+            _jwt = jwt;
+            
+            _users = new List<User>()
+            {
+                new User() { Id = 0, Role = "User", Username = "PleaseUseThisDot"},
+                new User() { Id = 1, Role = "User", Username = "JavaScriptIsKing"},
+                new User() { Id = 2, Role = "User", Username = "Test"},
+            };
+        }
+        
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult Login([FromBody] ApiLoginModel model)
+        {
+            var user = _users.FirstOrDefault(x => x.Username == model.Username);
+            
+            if (user == null) // TODO: Implement proper authentication in v2
+            {
+                return Unauthorized("Could not login with the provided credentials");
+            }
+
+            var jwt = _jwt.GenerateAccessToken(user);
+            return Ok(new ApiJwtModel() { access_token = jwt.Item1, expires_in = jwt.Item2});
+        }
+    }
+}
